2ad10fad9381f567bd05f1104eedaa297b99fac2
Grey Monroe <greymonroe@gmail.com>
2024-01-18 12:21:45 -0800

    m

--- sbatch/test.sbatch.sh
+++ sbatch/test.sbatch.sh
@@ -2,15 +2,12 @@
 #SBATCH -o /home/gmonroe/slurm-log2/%j-stdout.txt
 #SBATCH -e /home/gmonroe/slurm-log2/%j-stderr.txt
 #SBATCH -J bwa
-#SBATCH -t 72:00:00
-#SBATCH --mem 30G
-#SBATCH -n 8
+#SBATCH -t 96:00:00
+#SBATCH --ntasks=4
+#SBATCH --mem-per-cpu=32G
 #SBATCH --partition=bmh
 #SBATCH --mail-type=FAIL
-#SBATCH --mail-user=greymonroe@gmail.com
-set -e
-set -u
-set -xv
+#SBATCH --mail-user=gmonroe@ucdavis.edu
 
 REF=$1
 PREFIX=$2
