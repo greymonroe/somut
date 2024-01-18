55c5f54c11ac3d9c818a982804da99eb953a5586
Grey Monroe <greymonroe@gmail.com>
2024-01-18 09:50:50 -0800

    m

--- scripts_runall/1_trimmomatic_loop.sh
+++ scripts_runall/1_trimmomatic_loop.sh
@@ -15,8 +15,7 @@ for dir in "$parent_directory"/*; do
         echo "Processing: $dir_name"
         
         #READ1=$1, READ2=$2, PREFIX=$3, DIR=$4
-        #sbatch ../1_trimmomatic.sbatch.sh $dir/${dir_name}_L1_1.fq.gz $dir/${dir_name}_L1_2.fq.gz $dir_name ~/projects/atx_ko
-
+        sbatch ./somut/sbatch/1_trimmomatic.sbatch.sh ~/projects/atx_ko/0_raw/${dir_name}/${dir_name}_L1_1.fq.gz ~/projects/atx_ko/0_raw/${dir_name}/${dir_name}_L1_2.fq.gz $dir_name ~/projects/atx_ko
     fi
 done
 
@@ -24,8 +23,15 @@ parent_directory=~/projects/atx_ko/0_raw
 dir=/home/gmonroe/projects/atx_ko/0_raw/WT_1
 dir_name=$(basename "$dir")
 
-        echo "Processing: $dir_name"
+        echo "Processing: $dir"
+         #REF=$1, PREFIX=$2, DIR=$3
+        sbatch ./somut/sbatch/2_bwa.sbatch.sh ~/data/genome/a_thaliana/TAIR10_chr_all.fasta $dir_name ~/projects/atx_ko/0_raw
+        
         
         #READ1=$1, READ2=$2, PREFIX=$3, DIR=$4
         sbatch ./somut/sbatch/1_trimmomatic.sbatch.sh $dir/${dir_name}_L1_1.fq.gz $dir/${dir_name}_L1_2.fq.gz $dir_name ~/projects/atx_ko
+        #REF=$1, PREFIX=$2, DIR=$3
+        sbatch ./somut/sbatch/1_bwa.sbatch.sh ~/data/genome/a_thaliana/TAIR10_chr_all.fasta $dir_name ~/projects/atx_ko/0_raw
+
 ~/projects/atx_ko/0_raw/WT_1
+
