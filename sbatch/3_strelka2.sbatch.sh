630fd504173e417916d959ac40d673736cc84d6f
Grey Monroe <greymonroe@gmail.com>
2024-01-19 10:06:20 -0800

    m

--- sbatch/3_strelka2.sbatch.sh
+++ sbatch/3_strelka2.sbatch.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -l
 #SBATCH -o /home/gmonroe/slurm-log2/%j-stdout.txt
 #SBATCH -e /home/gmonroe/slurm-log2/%j-stderr.txt
-#SBATCH -J test
+#SBATCH -J strelka2
 #SBATCH -t 24:00:00
-#SBATCH --ntasks=4
+#SBATCH --ntasks=8
 #SBATCH --mem-per-cpu=32G
 #SBATCH --partition=bmh
 #SBATCH --mail-type=ALL
@@ -14,13 +14,15 @@ NORMAL=$2
 REF=$3
 DIR=$4
 
+module load strelka/2.9.10
 conda activate py2
-STRELKA_INSTALL_PATH=~/strelka-2.9.2.centos6_x86_64
+#STRELKA_INSTALL_PATH=~/strelka-2.9.2.centos6_x86_64
 
 mkdir -p ${DIR}/3_strelka/${TUMOR}/${NORMAL}
 rm -rf ${DIR}/3_strelka/${TUMOR}/${NORMAL}/*
 
-${STRELKA_INSTALL_PATH}/bin/configureStrelkaSomaticWorkflow.py \
+#${STRELKA_INSTALL_PATH}/bin/configureStrelkaSomaticWorkflow.py \
+$configureStrelkaSomaticWorkflow.py \
         --normalBam ${DIR}/2_bam/${NORMAL}.fix.markdup.bam \
         --tumorBam ${DIR}/2_bam/${TUMOR}.fix.markdup.bam \
         --referenceFasta $REF \
