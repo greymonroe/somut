82f289accdc1a7ceee99b6421c618bc3b13994e8
Grey Monroe <greymonroe@gmail.com>
2024-01-24 11:33:07 -0800

    n

--- sbatch/0_fastqc.sbatch.sh
+++ sbatch/0_fastqc.sbatch.sh
@@ -3,8 +3,8 @@
 #SBATCH -e /home/gmonroe/slurm-log2/%j-stderr.txt
 #SBATCH -J fastqc
 #SBATCH -t 96:00:00
-#SBATCH --ntasks=4
-#SBATCH --mem-per-cpu=32G
+#SBATCH --ntasks=1
+#SBATCH --mem-per-cpu=8G
 #SBATCH --partition=bmh
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=gmonroe@ucdavis.edu
